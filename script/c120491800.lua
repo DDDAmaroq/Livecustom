9438246b3c53a02b31d7323d1cedd3e5ce615115
DDDAmaroq <32921310+DDDAmaroq@users.noreply.github.com>
2017-10-29 11:40:01 -0400

    Pyrosprite Pyromancer reupload
    Script for Pyrosprite Pyromancer

--- script/c120491800.lua
+++ script/c120491800.lua
@@ -23,6 +23,24 @@ function c120491800.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetValue(c120491800.atkval)
 	c:RegisterEffect(e2)
+	--splimit
+	local e3=Effect.CreateEffect(c)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(c120491800.regcon)
+	e3:SetOperation(c120491800.regop)
+	c:RegisterEffect(e3)
+	--damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(2729285,0))
+	e4:SetCategory(CATEGORY_DAMAGE)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_BE_MATERIAL)
+	e4:SetCondition(c120491800.damcon)
+	e4:SetTarget(c120491800.damtg)
+	e4:SetOperation(c120491800.damop)
+	c:RegisterEffect(e4)
 end
 function c120491800.filter(c,e,tp,zone)
 	return ((c:IsAttribute(ATTRIBUTE_FIRE) and c:IsType(TYPE_NORMAL)) 
@@ -48,4 +66,33 @@ function c120491800.operation(e,tp,eg,ep,ev,re,r,rp)
 end
 function c120491800.atkval(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_GRAVE,0,nil,RACE_PYRO)*300
+end
+function c120491800.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_LINK)==SUMMON_TYPE_LINK
+end
+function c120491800.regop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTarget(c120491800.splimit)
+	Duel.RegisterEffect(e1,tp)
+end
+function c120491800.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsCode(120491800) and bit.band(sumtype,SUMMON_TYPE_LINK)==SUMMON_TYPE_LINK
+end
+function c120491800.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return r==REASON_LINK
+end
+function c120491800.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,0,0,tp,1000)
+end
+function c120491800.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
 end
